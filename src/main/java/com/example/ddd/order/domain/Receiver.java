d793e02f53e0beeb8e39b281dae5f1b1d3813f5f
skarltjr <kisa0828@naver.com>
2022-11-28 18:32:04 +0900

    feat : 받는이, 배송지 생성자 추가

--- src/main/java/com/example/ddd/order/domain/Receiver.java
+++ src/main/java/com/example/ddd/order/domain/Receiver.java
@@ -3,4 +3,10 @@ package com.example.ddd.order.domain;
 public class Receiver {
     private String name;
     private String phone;
+
+    public Receiver(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
 }
+
--- src/main/java/com/example/ddd/order/domain/ShippingInfo.java
+++ src/main/java/com/example/ddd/order/domain/ShippingInfo.java
@@ -5,4 +5,9 @@ public class ShippingInfo {
     private Receiver receiver;
     private String message;
 
+    public ShippingInfo(Address address, Receiver receiver, String message) {
+        this.address = address;
+        this.receiver = receiver;
+        this.message = message;
+    }
 }
