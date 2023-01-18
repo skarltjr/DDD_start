955311091b75bb5cabb02ef317b31e3e80859c4d
skarltjr <kisa0828@naver.com>
2023-01-18 10:06:31 +0900

    chore : add product error message

--- src/main/java/com/example/ddd/catalog/ErrorCodes.java
+++ src/main/java/com/example/ddd/catalog/ErrorCodes.java
@@ -4,4 +4,5 @@ public class ErrorCodes {
     public static final String NOT_VALID_PRODUCT_CATEGORIES = "유효하지 않은 상품 카테고리입니다.";
     public static final String ALREADY_EXIST_CATEGORY = "이미 존재하는 카테고리입니다.";
     public static final String IMAGE_NOT_EXIST = "이미지가 존재하지 않습니다.";
+    public static final String PRODUCT_NOT_FOUND = "존재하지 않는 상품입니다.";
 }
